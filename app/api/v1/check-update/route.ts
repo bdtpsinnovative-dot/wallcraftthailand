dda74e6a8543649af9adb38d22f4236cd39ff4de
twanty979-hue <twanty979@gmail.com>
2026-08-19 17:41:36 +0700

    2.0.2

--- app/api/v1/check-update/route.ts
+++ app/api/v1/check-update/route.ts
@@ -6,10 +6,10 @@ export async function GET() {
       // เอาไว้สำหรับแอปเวอร์ชันเก่าที่ยังไม่ได้อัปเดตโค้ดใหม่ จะได้ไม่พัง
       latest_version: "2.0.0", 
       // แยก Version ของ Android และ iOS (สำหรับแอปเวอร์ชันใหม่)
-      latest_version_android: "2.0.1",
+      latest_version_android: "2.0.2",
       latest_version_ios: "2.0.0",
       // แยก Link ของ Android (APK) และ iOS (TestFlight)
-      download_url_android: "https://app.wallcraftthailand.com/wallcraft2.0.1.apk", 
+      download_url_android: "https://app.wallcraftthailand.com/app-release.apk", 
       download_url_ios:  "https://testflight.apple.com/join/BXMvdwVM", 
       release_date: "2026-05-12",
       change_log: "เพิ่มระบบตรวจสอบการอัปเดตอัตโนมัติ และปรับปรุงประสิทธิภาพแอป เพิ่มระบบสอตกสินค้าและติดตามสินค้า"
